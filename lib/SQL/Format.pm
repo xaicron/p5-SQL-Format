f57b4d86c4ba9ae770b6a404029d9dc0087df0a3
xaicron <xaicron@gmail.com>
2011-11-10 02:29:21 +0900

    initial commit

---
+++ lib/SQL/Format.pm
@@ -0,0 +1,42 @@
+package SQL::Format;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+SQL::Format -
+
+=head1 SYNOPSIS
+
+  use SQL::Format;
+
+=head1 DESCRIPTION
+
+SQL::Format is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
